494bfb5bea212c9c3f144ac050ad0d69d9d1a815
[a613363] Yassine El Mardi <yassine.elmardi@atos.net>
2018-03-20 18:39:29 +0000

    Ajout des composant depuis le site officiel primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ConfirmationService} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private confirmationService: ConfirmationService) {}
+
+  display: boolean = false;
+
+  showDialog() {
+    this.display = true;
+  }
 }
